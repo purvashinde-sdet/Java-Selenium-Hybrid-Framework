ce2a337892b5a7b7f90213c3b08d0b8dfea96ee1
purvashinde-sdet <purvashinde06062001@gmail.com>
2026-02-24 16:22:14 +0530

    Create GoogleTest.java

---
+++ src/test/java/tests/GoogleTest.java
@@ -0,0 +1,20 @@
+package tests;
+
+import base.BaseTest;
+import org.testng.annotations.Test;
+
+public class GoogleTest extends BaseTest {
+
+@Test
+
+public void verifyGoogle(){
+
+setup();
+
+System.out.println("Automation Framework Working");
+
+tearDown();
+
+}
+
+}
