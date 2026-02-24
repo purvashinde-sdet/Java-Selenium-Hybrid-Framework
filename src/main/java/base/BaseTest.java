74e417d067058d139739fe6e3877981c6efb40eb
purvashinde-sdet <purvashinde06062001@gmail.com>
2026-02-24 16:19:44 +0530

    Create BaseTest.java

---
+++ src/main/java/base/BaseTest.java
@@ -0,0 +1,26 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTest {
+
+public static WebDriver driver;
+
+public void setup(){
+
+driver = new ChromeDriver();
+
+driver.manage().window().maximize();
+
+driver.get("https://www.google.com");
+
+}
+
+public void tearDown(){
+
+driver.quit();
+
+}
+
+}
